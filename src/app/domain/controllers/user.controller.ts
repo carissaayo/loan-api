2de0e3695bc068224392679242257820eac20052
Yussuf <you@example.com>
2025-03-18 10:58:46 +0100

    remove the authguard, it seems to be working without adding it to the controllers

--- src/app/domain/controllers/user.controller.ts
+++ src/app/domain/controllers/user.controller.ts
@@ -2,20 +2,17 @@ import {
   Controller,
   Get,
   Param,
-  Post,
   Body,
   Patch,
   UseGuards,
   Req,
 } from '@nestjs/common';
 import { UsersService } from '../services/user.service';
-import { JwtAuthGuard } from 'src/app/auth/jwt.guard';
 import { AuthenticatedRequest, RolesGuard } from '../middleware/role.guard';
 import { Roles } from '../middleware/role.decorator';
 import { Role } from '../enums/roles.enum';
 
 @Controller('users')
-// @UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
